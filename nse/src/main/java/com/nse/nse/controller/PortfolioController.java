10bc43c96c990e63e743cbbabe6d783ee9630c32
shubham bondre <shubhambondre1997@gmail.com>
2022-02-24 15:43:08 +0530

    added changes to initial commit

--- nse/src/main/java/com/nse/nse/controller/PortfolioController.java
+++ nse/src/main/java/com/nse/nse/controller/PortfolioController.java
@@ -8,12 +8,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.nse.nse.entity.Portfolio;
-
+import com.nse.nse.entity.Share;
 import com.nse.nse.service.Service;
 
 @RestController
@@ -23,28 +24,32 @@ public class PortfolioController {
 private Service service;
 
 	
-	@RequestMapping("/createPortfolio")
+	@PostMapping("/createPortfolio")
 	public ResponseEntity<Portfolio> createPortfolio(@RequestBody Portfolio p) {
 		return new ResponseEntity<>(service.createPortfolio(p),HttpStatus.OK);
 		
 		
 	}
-	@RequestMapping("/buyShare/{shareId}/{portfolioId}")
+	@PostMapping("/buyShare/{shareId}/{portfolioId}")
 		public boolean buyShare(@PathVariable Integer shareId,@PathVariable Integer portfolioId ) {
 			return service.buyShare(shareId,portfolioId);
 			
 		}
 		
-	@RequestMapping("/sellShare/{shareId}/{portfolioId}")
+	@PostMapping("/sellShare/{shareId}/{portfolioId}")
 	public boolean sellShare(@PathVariable Integer shareId,@PathVariable Integer portfolioId ) {
 		return service.sellShare(shareId,portfolioId);
 		
 	}
-		@RequestMapping("/showPortfolio")
+		@PostMapping("/showPortfolio")
 		public ResponseEntity<List<Portfolio>> showPortfolio() {
 			return new ResponseEntity<>(service.showPortfolio(),HttpStatus.OK);
 			
 			
+		}
+		@RequestMapping("/createShare")
+		public ResponseEntity<Share> createShare(@RequestBody Share s) {
+			return new ResponseEntity<>(service.createShare(s), HttpStatus.OK);
 		}
 		
 		
--- nse/src/main/java/com/nse/nse/service/Service.java
+++ nse/src/main/java/com/nse/nse/service/Service.java
@@ -4,55 +4,24 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 
 import com.nse.nse.entity.Portfolio;
 import com.nse.nse.entity.Share;
-import com.nse.nse.repository.PortfolioRepo;
-import com.nse.nse.repository.ShareRepo;
 
 @org.springframework.stereotype.Service
-public class Service {
+public interface Service {
 
-	@Autowired
-	private PortfolioRepo pRepo;
-	
-	@Autowired
-	private ShareRepo sRepo;
-	
-	public Portfolio createPortfolio(Portfolio p) {
-		return pRepo.save(p);
-	}
-	public List<Portfolio> showPortfolio(){
-		return pRepo.findAll();
-	}
-	
-	public boolean buyShare(Integer shareId,Integer portfolioId) {
-		Portfolio p= pRepo.findById(portfolioId).get();
-		Share s= sRepo.findById(shareId).get();
-		p.setInvestment (p.getInvestment ()+ s.getPrice());
-		List<Portfolio>plist=new ArrayList<>();
-		plist.add(p);
-		List<Share>slist=new ArrayList<>();
-		slist.add(s);
-	p.setShares(slist);	
-	s.setPortfolio(plist);
-	pRepo.save(p);
-	sRepo.save(s);
-	return true;
-	}
+	public Portfolio createPortfolio(Portfolio p);
 
-public boolean sellShare(Integer shareId,Integer portfolioId) {
-	Portfolio p= pRepo.findById(portfolioId).get();
-	Share s= sRepo.findById(shareId).get();
-	p.setInvestment (p.getInvestment ()- s.getPrice());
-	List<Portfolio>plist=new ArrayList<>();
-	plist.add(p);
-	List<Share>slist=new ArrayList<>();
-	slist.add(s);
-p.setShares(slist);	
-s.setPortfolio(plist);
-pRepo.delete(p);
-sRepo.delete(s);
-return true;
-}
+	public List<Portfolio> showPortfolio();
+
+	public boolean buyShare(Integer shareId, Integer portfolioId);
+
+	public boolean sellShare(Integer shareId, Integer portfolioId);
+
+	public List<Share> showShare();
+
+	public Share createShare(Share s);
+
 }
